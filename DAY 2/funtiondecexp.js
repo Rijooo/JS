56be33e10a643e0be6b488b671b5e9cf2cce245b
Rijooo <connectrijoben@gmail.com>
2024-05-21 14:59:40 +0530

    hoisting

---
+++ DAY 2/funtiondecexp.js
@@ -0,0 +1,29 @@
+// function declaration 
+
+myLikes();
+function myLikes(){
+    console.log("kumari");
+}
+
+
+
+// function expression
+
+let like = function(){
+    console.log("kumari2")
+}
+like();
+
+
+
+// arrow function
+
+let one = () =>{
+    console.log("kumari3")
+}
+one();
+
+
+
+// only function declaration is hoisted 
+// function expression and arrow function are not hoisted
---
+++ DAY 2/hoist.js
@@ -0,0 +1,24 @@
+
+
+//declaring using var
+console.log(a);
+var a = 10;
+
+
+//declaring using let
+
+
+let b = 20;
+console.log(b);
+
+
+//declaring using const
+
+
+console.log(c);
+const c = 30;
+
+
+
+// var is hosited
+//let and const are not hoisted
