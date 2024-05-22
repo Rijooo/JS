10460d7b239d7a7a3630b19e240feef7c5f02043
Rijooo <connectrijoben@gmail.com>
2024-05-22 11:03:05 +0530

    obj

--- DAY 3/obj.js
+++ DAY 3/obj.js
@@ -1,26 +1,20 @@
-
-
 // one method to create an object or called creating object using literal
 
 let one = {
-name:"Rijo",
-age:24,
-must:{first :"house", second: "VN"},
-extra:["ngo","ground"]
+  name: "Rijo",
+  age: 24,
+  must: { first: "house", second: "VN" },
+  extra: ["ngo", "ground"],
+};
 
-}
-
-console.log(one.extra[0])
-
+console.log(one.extra[0]);
 
 // object creation using new keywor or using instance
 
+let two = new Object();
+two.name = "Rijo";
+two.city = "kumari";
+two.age = 24;
+(two.must = { first: "house", second: "VN" }), (two.extra = ["ngo", "ground"]);
 
-let two = new Object()
-two.name="Rijo"
-two.city="kumari"
-two.age=24
-two.must={first :"house", second: "VN"},
-two.extra=["ngo","ground"]
-
-console.log(two)
+console.log(two);
