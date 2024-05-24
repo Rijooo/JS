d79b48b468e6c4b3cf6ce87051df9d05fc7fcd87
Rijooo <connectrijoben@gmail.com>
2024-05-24 11:55:57 +0530

    extension

--- DAY 3/extensible.js
+++ DAY 3/extensible.js
@@ -1,13 +1,31 @@
-let a ={
-  name : "Rijo",
-  age: "24",
-  city:"kumari",
-  x: function(){
-    console.log("The city is beautiful")
-  }
 
+
+let a ={
+    name : "Rijo",
+    age : "24",
+    city:"Kumari",
+    func: function(){
+        console.log("KUMARI is beautiful")
+    }
 }
+// a.func()
 
 console.log(a)
 
-console.log(Object.isExtensible(a))
+console.log(Object.isExtensible(a));
+
+Object.preventExtensions(a)
+
+try{
+Object.defineProperty(a,"property1",{foot:"cristiano"})
+}
+catch(e){
+    console.log(e)
+}
+
+//a.foot="cristiano"
+//console.log(Object.isExtensible(a));
+
+console.log(a)
+
+
