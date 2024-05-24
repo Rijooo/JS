92e93a15eba73da719a6d7211ef9ed5d3bc92761
Rijooo <connectrijoben@gmail.com>
2024-05-24 16:07:46 +0530

    array

---
+++ DAY 5/array.js
@@ -0,0 +1,16 @@
+// creation of array using literal
+
+let arr = ["kumari", 1, "tutu", 2, "cbe", 3];
+
+console.log(arr);
+console.log(typeof arr);
+
+console.log(arr.length);
+
+//creatio using new keyword
+
+let arr1 = new Array("kanyakumari", 2, "kumari", 2);
+
+console.log(arr1)
+
+console.log(arr1.sort())
