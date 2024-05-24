ed53480fa7069e9bcab33160ed14b6feb661d789
Rijooo <connectrijoben@gmail.com>
2024-05-24 09:26:28 +0530

    extensible

---
+++ DAY 3/extensible.js
@@ -0,0 +1,13 @@
+let a ={
+  name : "Rijo",
+  age: "24",
+  city:"kumari",
+  x: function(){
+    console.log("The city is beautiful")
+  }
+
+}
+
+console.log(a)
+
+console.log(Object.isExtensible(a))
