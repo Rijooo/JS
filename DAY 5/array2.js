78559312e8ae5ef315e7174453d335e824596161
Rijooo <connectrijoben@gmail.com>
2024-05-27 09:42:48 +0530

    array

---
+++ DAY 5/array2.js
@@ -0,0 +1,8 @@
+// let arr = ["I", "am", "so", "sorry"];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+
+let arr1 = new Array("I","am","so","hungry")
+arr1.forEach((index,items) => console.log(index,items) )
