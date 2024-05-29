d4bd947bba9a9d4cc1140d2e415cf890aa3fea53
Rijooo <connectrijoben@gmail.com>
2024-05-29 10:38:58 +0530

    settime

---
+++ DAY 8/settiout.js
@@ -0,0 +1,13 @@
+function x(a){
+    let name= "Rijo"
+    console.log(name + a)
+}
+
+setTimeout(x,3000,10)
+
+//another way
+
+setTimeout((b)=>{
+    let name="Asmi"
+    console.log(name+ b)
+},2000,20)
