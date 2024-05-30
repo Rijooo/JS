1bbc469ef818d8914ebd7d3a4f5df8a9873aaaeb
Rijooo <connectrijoben@gmail.com>
2024-05-30 20:11:08 +0530

    promise

---
+++ DAY 9/promises.js
@@ -0,0 +1,27 @@
+
+let neww="RIJOO"
+console.log(neww.includes("R"))
+
+
+
+let p = new Promise(function (resolve, reject) {
+  let name = "RIJO";
+  if (name.includes("R") == true) {
+    
+    resolve();
+  } //this whole block is called executer
+  else {
+    reject();
+  }
+});
+
+p.then(function () {
+  console.log("resolved");
+}).catch(function () {
+  console.log("rejected");
+});
+
+
+
+
+let promise= new Promise((resolved,rejected)=>{}).then(()=>{}).catch(()=>{})
